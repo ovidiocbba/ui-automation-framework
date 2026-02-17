5480ebd00434b8a1bd73866efb8511a177dc2280
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-02-17 15:23:43 -0400

    feat(core-driver): add DriverType enum

---
+++ src/main/java/com/ovidiomiranda/framework/core/driver/DriverType.java
@@ -0,0 +1,12 @@
+package com.ovidiomiranda.framework.core.driver;
+
+/**
+ * Enum for the Driver types.
+ *
+ * @author Ovidio Miranda
+ */
+public enum DriverType {
+  CHROME,
+  EDGE,
+  FIREFOX
+}
