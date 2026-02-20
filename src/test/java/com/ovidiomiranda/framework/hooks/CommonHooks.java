d8373d1cab3f32664458cfc30c09e131aeaf3a3d
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-02-19 21:35:35 -0400

    feat(hooks): attach screenshot to Allure report on scenario failure

--- src/test/java/com/ovidiomiranda/framework/hooks/CommonHooks.java
+++ src/test/java/com/ovidiomiranda/framework/hooks/CommonHooks.java
@@ -2,6 +2,11 @@ package com.ovidiomiranda.framework.hooks;
 
 import com.ovidiomiranda.framework.core.driver.DriverManager;
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
+import java.io.ByteArrayInputStream;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /**
  * Class containing Common Hooks.
@@ -11,10 +16,17 @@ import io.cucumber.java.After;
 public class CommonHooks {
 
   /**
-   * Closes the WebDriver instance after each scenario execution.
+   * After hook: attaches screenshot on failure and closes WebDriver.
+   *
+   * @param scenario current scenario
    */
   @After
-  public void tearDown() {
+  public void tearDown(Scenario scenario) {
+    if (scenario.isFailed()) {
+      byte[] screenshot = ((TakesScreenshot) DriverManager.getInstance()
+          .getDriver()).getScreenshotAs(OutputType.BYTES);
+      Allure.addAttachment(scenario.getName(), new ByteArrayInputStream(screenshot));
+    }
     DriverManager.getInstance().quitDriver();
   }
 }
