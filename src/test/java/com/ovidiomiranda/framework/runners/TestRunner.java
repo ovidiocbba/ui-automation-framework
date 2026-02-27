aa3d7970876d4f241656b9043432a5058a99dd5c
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-02-27 12:38:43 -0400

    feat(tests): enable parallel execution in TestRunner

--- src/test/java/com/ovidiomiranda/framework/runners/TestRunner.java
+++ src/test/java/com/ovidiomiranda/framework/runners/TestRunner.java
@@ -2,6 +2,7 @@ package com.ovidiomiranda.framework.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 /**
  * Test Runner class responsible for executing Cucumber feature files.
@@ -18,4 +19,14 @@ import io.cucumber.testng.CucumberOptions;
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
+  /**
+   * Overrides the default scenarios provider to enable parallel
+   * execution of Cucumber scenarios using TestNG.
+   * Each scenario is executed in a separate thread.
+   */
+  @Override
+  @DataProvider(parallel = true)
+  public Object[][] scenarios() {
+    return super.scenarios();
+  }
 }
