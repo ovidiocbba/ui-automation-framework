73f5f2ff8edc876e46d8633531421f6a1a35bac2
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-11 22:02:45 -0400

    docs(workspace): update comments for supported browsers list

--- vars/prepareWorkspace.groovy
+++ vars/prepareWorkspace.groovy
@@ -1,6 +1,6 @@
 def call() {
     cleanWs()
-    // Define the list of supported browsers inside a script block
+    // Define the list of supported browsers
     def ALL_BROWSERS = ['CHROME_HEADLESS', 'FIREFOX_HEADLESS', 'EDGE_HEADLESS']
     return params.BROWSER == 'ALL' ? ALL_BROWSERS : [params.BROWSER]
 }
